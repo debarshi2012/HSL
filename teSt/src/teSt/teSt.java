e0e7688054ab8d73f7574a551566cf8b11f0132e
asus <asus@DESKTOP-89LLJVF>
2020-02-19 23:48:49 +0530

    added change

--- teSt/src/teSt/teSt.java
+++ teSt/src/teSt/teSt.java
@@ -2,7 +2,8 @@ package teSt;
 
 public class teSt {
 	public static void main(String[] args) {
-		System.out.println("hello");	
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 	
 }
